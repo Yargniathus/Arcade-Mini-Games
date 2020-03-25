c50b2bc1f7799c5508e752d57e8a28812c2bc06f
Jani Saari <hedcker@gmail.com>
2020-03-25 09:42:57 +0000

    Initial labyrinthe changes

---
+++ Assets/Labyrintti/Scripts/MovementScriptLabyrintti.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScriptLabyrintti : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.position = new Vector2(this.transform.position.x+1, this.transform.position.y);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.transform.position = new Vector2(this.transform.position.x + -1, this.transform.position.y);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y-1);
+        }
+    }
+}
