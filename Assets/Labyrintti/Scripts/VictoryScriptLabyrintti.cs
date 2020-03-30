f435988e389845d729e37a700531261d2446449a
Jani Saari <hedcker@gmail.com>
2020-03-30 08:10:14 +0000

    Wall, victory condition

---
+++ Assets/Labyrintti/Scripts/VictoryScriptLabyrintti.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryScriptLabyrintti : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+          
+            SceneManager.LoadScene("LabyrinttiVictory");
+        }
+    }
+}
