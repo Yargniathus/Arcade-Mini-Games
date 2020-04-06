8197c2dd9e44bf1eed9296c1e09a99c5fd31c209
Jani Saari <hedcker@gmail.com>
2020-04-06 12:41:42 +0000

    Continuing random scene (lacking water)

---
+++ Assets/Labyrintti/Scripts/RandomTileLabyrintti.cs
@@ -0,0 +1,202 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RandomTileLabyrintti : MonoBehaviour
+{
+    public Tilemap GroundTileMap;
+    public Tilemap WaterTileMap;
+    public Tilemap FinishTileMap;
+    public Tile VerticalTile;
+    public Tile HorizontalRightToLeft;
+    public Tile HorizontalLeftToRight;
+    public Tile DownToRightTile;
+    public Tile DownToLeftTile;
+    public Tile LeftToDownTile;
+    public Tile RightToDownTile;
+    public Tile WaterTile;
+    public Tile FinishTile;
+    public static Tile EmptyTile;
+    private int yValue;
+    private int xValue;
+    public Tile[,] TileList;
+    public Tile TileToBePlaced;
+    public Tile LastPlacedTile;
+    private int yToArrayValue;
+    private int leftEdgeValue;
+    private int rightEdgeValue;
+    private bool allTilesGenerated;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xValue = -1;
+        yValue = 2;
+        leftEdgeValue = -3;
+        rightEdgeValue = 0;
+        LastPlacedTile = EmptyTile;
+        yToArrayValue = yValue * -1 + 2;
+        allTilesGenerated = false;
+        TileList = new Tile[6, 6] {
+        {EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile } ,
+        {EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile } ,
+        {EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile } ,
+        {EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile } ,
+        {EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile } ,
+        {EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile, EmptyTile }
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && allTilesGenerated!=true)
+        {
+                      
+            yToArrayValue = yValue * -1 + 2;
+            int xToArrayValue = xValue+3;
+            
+            RandomizePiece();
+
+            
+            GroundTileMap.SetTile(new Vector3Int(xValue, yValue, 0), TileToBePlaced);
+            TileList.SetValue(TileToBePlaced, yToArrayValue, xToArrayValue);
+            LastPlacedTile = TileToBePlaced;
+            if (LastPlacedTile==VerticalTile || LastPlacedTile == LeftToDownTile || LastPlacedTile == RightToDownTile)
+            {
+                yValue -= 1;
+                if (yValue == -3)
+                {
+                    yToArrayValue = yValue * -1 + 2;
+                    TileList.SetValue(FinishTile, yToArrayValue, xToArrayValue);
+                    FinishTileMap.SetTile(new Vector3Int(xValue, yValue, 0), FinishTile);
+                    //foreach (Tile tiles in TileList)
+                    //{
+                    //    if (tiles == EmptyTile)
+                    //    {
+                    //        Debug.Log("EmptytileFoudn");
+                    //    }
+                    //}
+                    allTilesGenerated = true;
+                }
+                
+            }
+            if (LastPlacedTile==HorizontalRightToLeft || LastPlacedTile== DownToLeftTile)
+            {
+                xValue -= 1;
+            }
+            if(LastPlacedTile==HorizontalLeftToRight || LastPlacedTile == DownToRightTile)
+            {
+                xValue += 1;
+            }
+                
+          
+        }
+    }
+    private void RandomizePiece()
+    {
+        if (LastPlacedTile==EmptyTile || LastPlacedTile == VerticalTile || LastPlacedTile == LeftToDownTile || LastPlacedTile == RightToDownTile)
+        {
+            if (xValue < rightEdgeValue && xValue > leftEdgeValue)
+            {
+                int rnd = Random.Range(1, 4);
+                switch (rnd)
+                {
+                    case 1:
+                        TileToBePlaced = VerticalTile;
+                        break;
+
+                    case 2:
+                        if (LastPlacedTile != LeftToDownTile)
+                        {
+                            TileToBePlaced = DownToLeftTile;
+                        }
+                        else { TileToBePlaced = VerticalTile; }
+                        break;
+                    case 3:
+                        if (LastPlacedTile != RightToDownTile)
+                        {
+                            TileToBePlaced = DownToRightTile;
+                        }
+                        else { TileToBePlaced = VerticalTile; }
+                        break;
+                }
+            }
+            else if(xValue == rightEdgeValue)
+            {
+                int rnd = Random.Range(1, 3);
+                switch (rnd)
+                {
+                    case 1:
+                        TileToBePlaced = VerticalTile;
+                        break;
+                    case 2:
+                        if (LastPlacedTile == EmptyTile || LastPlacedTile == VerticalTile)
+                        {
+                            TileToBePlaced = DownToLeftTile;
+                        }
+                        else TileToBePlaced = VerticalTile;
+                        break;
+                }
+            } else { int rnd = Random.Range(1, 3);
+            switch (rnd)
+                {
+                    case 1:
+                    TileToBePlaced = VerticalTile;
+                    break;
+
+                    case 2:
+                        if (LastPlacedTile == EmptyTile || LastPlacedTile == VerticalTile)
+                        {
+                            TileToBePlaced = DownToRightTile;
+                        }
+                        else TileToBePlaced = VerticalTile;
+                    break;
+                }
+                    }
+        }
+        if (LastPlacedTile == HorizontalRightToLeft || LastPlacedTile == DownToLeftTile)
+        {
+            
+            if (xValue > leftEdgeValue)
+            {
+                int rnd = Random.Range(1, 3);
+                switch (rnd)
+                {
+                    case 1:
+                        TileToBePlaced = HorizontalRightToLeft;
+                        break;
+
+                    case 2:
+                        TileToBePlaced = RightToDownTile;
+                        break;
+                }
+            }
+            else { TileToBePlaced = RightToDownTile; }
+        }
+        if (LastPlacedTile == HorizontalLeftToRight || LastPlacedTile == DownToRightTile)
+        {
+            
+            if (xValue < rightEdgeValue)
+            {
+                
+                int rnd = Random.Range(1, 3);
+                switch (rnd)
+                {
+                    case 1:
+                        TileToBePlaced = HorizontalLeftToRight;
+                        break;
+
+                    case 2:
+                        TileToBePlaced = LeftToDownTile;
+                        break;
+                }
+            }
+            else { TileToBePlaced = LeftToDownTile; }
+        }
+
+    }
+
+
+}
