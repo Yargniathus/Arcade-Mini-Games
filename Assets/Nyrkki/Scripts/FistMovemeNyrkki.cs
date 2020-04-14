af7e569b57706a763588395107b400843e0d7896
Jani Saari <hedcker@gmail.com>
2020-04-14 08:44:52 +0000

    Placeholders for Nyrkki

---
+++ Assets/Nyrkki/Scripts/FistMovemeNyrkki.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FistMovemeNyrkki : MonoBehaviour
+{
+    bool LeftMovingUp;
+    bool RightMovingUp;
+    bool MovingMiddle;
+    public GameObject LeftFist;
+    public GameObject RightFist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeftMovingUp = false;
+        RightMovingUp = false;
+        MovingMiddle = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            LeftPunch();
+        }
+    }
+
+    void LeftPunch()
+    {
+        LeftMovingUp = true;
+        
+    }
+}
