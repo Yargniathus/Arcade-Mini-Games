11e3b3c2d9e00137afd83e3fb41649d250852d82
Jani Saari <hedcker@gmail.com>
2020-04-14 11:22:00 +0000

    Fist movement

--- Assets/Nyrkki/Scripts/FistMovemeNyrkki.cs
+++ Assets/Nyrkki/Scripts/FistMovemeNyrkki.cs
@@ -22,13 +22,68 @@ public class FistMovemeNyrkki : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            LeftPunch();
+            LeftMovingUp = true;
+        } 
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            RightMovingUp = true;
         }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            MovingMiddle = true;
+        }
+        LeftPunch();
+        RightPunch();
+        MiddlePunch();
     }
 
     void LeftPunch()
     {
-        LeftMovingUp = true;
-        
+        if (LeftFist.GetComponent<Transform>().position.y < 0.5 && LeftMovingUp == true)
+        {
+            LeftFist.transform.Translate(new Vector3(0, 25f * Time.deltaTime, 0));
+        }
+        if (LeftFist.GetComponent<Transform>().position.y >= 0.5)
+        {
+            LeftMovingUp = false;
+        }
+        if (LeftMovingUp == false && LeftFist.GetComponent<Transform>().position.y > -3.5)
+        {
+            LeftFist.transform.Translate(new Vector3(0, -25f * Time.deltaTime, 0));
+        }
     }
+    void RightPunch()
+    {
+        if (RightFist.GetComponent<Transform>().position.y < 0.5 && RightMovingUp == true)
+        {
+            RightFist.transform.Translate(new Vector3(0, 25f * Time.deltaTime, 0));
+        }
+        if (RightFist.GetComponent<Transform>().position.y >= 0.5)
+        {
+            RightMovingUp = false;
+        }
+        if (RightMovingUp == false && RightFist.GetComponent<Transform>().position.y > -3.5)
+        {
+            RightFist.transform.Translate(new Vector3(0, -25f * Time.deltaTime, 0));
+        }
+    }
+    void MiddlePunch()
+    {
+        if (LeftFist.GetComponent<Transform>().position.x < -1 && MovingMiddle == true)
+        {
+            LeftFist.transform.Translate(new Vector3(-25f * Time.deltaTime,0,  0));
+            RightFist.transform.Translate(new Vector3(-25f * Time.deltaTime, 0, 0));
+        }
+        if (LeftFist.GetComponent<Transform>().position.x >= -1)
+        {
+            MovingMiddle = false;
+        }
+        if (MovingMiddle == false && LeftFist.GetComponent<Transform>().position.x > -5)
+        {
+            LeftFist.transform.Translate(new Vector3(25f * Time.deltaTime, 0, 0));
+            RightFist.transform.Translate(new Vector3(25f * Time.deltaTime, 0, 0));
+        }
+    }
+
+
 }
