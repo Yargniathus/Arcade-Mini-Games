c4fcbff2bfe3caa11f4889ac0d3982652a77a962
Jani Saari <hedcker@gmail.com>
2020-05-28 14:17:16 +0000

    Added language and machine selection to first screen, continue button to get to game selection.

---
+++ Assets/MainMenu/Scripts/LanguageContinue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LanguageContinue : MonoBehaviour
+{
+    public Button continueButton;
+
+    void Start()
+    {
+        Button btn = continueButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
